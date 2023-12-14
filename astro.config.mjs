fa004db47ca7f205d0ae4a232b3f0f62d8ac8a8b
ybw0014 <i@guizhanss.com>
2023-12-13 20:08:26 -0500

    feat: setup template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,63 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+import tailwind from '@astrojs/tailwind'
+import cloudflare from '@astrojs/cloudflare'
+import remarkHeadingId from 'remark-heading-id'
+import rehypeExternalLinks from 'rehype-external-links'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+  integrations: [starlight({
+    title: 'Starlight Template',
+    description: 'Starlight Template',
+    // logo: {
+    //   src: './src/assets/logo.png'
+    // },
+    // favicon: '/favicon.ico',
+    customCss: [
+      '@fontsource/noto-sans',
+      '@fontsource/noto-sans-sc',
+      './src/styles/docs.css'
+    ],
+    social: {
+      github: 'https://github.com/ybw0014/starlight-template'
+    },
+    defaultLocale: 'root',
+    locales: {
+      root: {
+        label: 'English',
+        lang: 'en'
+      },
+      'zh-cn': {
+        label: '简体中文',
+        lang: 'zh-CN'
+      }
+    },
+    head: [
+      // uncomment the following to enable cloudflare analytics
+      // {
+      //   tag: 'script',
+      //   attrs: {
+      //     src: 'https://static.cloudflareinsights.com/beacon.min.js',
+      //     'data-cf-beacon': '{"token": ""}',
+      //     defer: true
+      //   }
+      // }
+    ],
+    sidebar: [
+      // TODO: Replace sidebar with your own
+    ]
+  }), tailwind({
+    applyBaseStyles: false
+  })],
+  markdown: {
+    remarkPlugins: [
+      remarkHeadingId
+    ],
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+    ]
+  },
+  output: 'static',
+  adapter: cloudflare()
+})
